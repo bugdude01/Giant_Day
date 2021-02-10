34309835a42e1de458376534f2c67d2962e368ac
Richard Rhodes <rafrhodes@yahoo.co.uk>
2021-02-10 17:41:42 +0000

    add topic model to comment on

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    started = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.user)
